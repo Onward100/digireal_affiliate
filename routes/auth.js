1b0815d359910024114a322c393244ffae95d4b1
Woruka Onward Onyekachi <worukaonward@gmail.com>
2025-07-01 19:51:25 +0100

    commit

---
+++ routes/auth.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.js");
+
+const router = express.Router();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// Sign Up
+router.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) return res.status(400).json({ msg: "User already exists" });
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ name, email, password: hashedPassword });
+  await newUser.save();
+
+  res.status(201).json({ msg: "User created successfully" });
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ msg: "Invalid credentials" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+  const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: "1h" });
+
+  res.json({
+    token,
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const connectDB = require('./config/connectDB');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const router = require('./routes/login');
+const express = require("express");
+const connectDB = require("./config/connectDB");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const router = require("./routes/auth");
 const app = express();
 dotenv.config();
 
 connectDB();
 
-app.use(cors([
+app.use(
+  cors([
     {
-        origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true
-    }
-]));
+      origin: process.env.CORS_ORIGIN || "http://localhost:5000" || "http://localhost:3000" ,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    },
+  ])
+);
 app.use(express.json());
 
-app.use("/api/login", router)
+app.use("/api/auth", router);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 5000}`);
+});
